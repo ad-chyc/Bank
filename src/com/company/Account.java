0fd42290f3cca08b190ab3d5c198bd16b84820aa
adchyc <adchyc@gmail.com>
2019-02-02 11:50:44 +0100

    first

---
+++ src/com/company/Account.java
@@ -0,0 +1,58 @@
+package com.company;
+
+public abstract class Account {
+    private String name;
+    private String surName;
+    private long account;
+    private long deposit;
+    private long withdrawals;
+
+    public enum typeAccount{
+        PERSONAL,
+        COMPANY
+    }
+
+    public Account(String name, String surName, long account, long deposit, long withdrawals) {
+        this.name = name;
+        this.surName = surName;
+        this.account = account;
+        this.deposit = deposit;
+        this.withdrawals = withdrawals;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
+
+    public long getAccount() {
+        return account;
+    }
+
+    public long getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(long deposit) {
+        this.deposit = deposit;
+    }
+
+    public long getWithdrawals() {
+        return withdrawals;
+    }
+
+    public void setWithdrawals(long withdrawals) {
+        this.withdrawals = withdrawals;
+    }
+}
